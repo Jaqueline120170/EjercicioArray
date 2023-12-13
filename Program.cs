72f2068f8447a474eb99d39e5cf5c8c0e6dedcb3
Jaqueline <csi23-jromres@colegioaltair.net>
2023-12-13 09:38:19 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EjercicioArray
+{
+    public static class Program
+    {
+        public static elementoVajilla[] Append<elementoVajilla>(this elementoVajilla[] array, elementoVajilla item)
+        {
+            List<elementoVajilla> list = new List<elementoVajilla>(array);
+            list.Add(item);
+
+            return list.ToArray();
+        }
+    }
+
+    public class aniadir
+    {
+        public static void Main()
+        {
+            int[] array = { 1, 2, 3, 4 };
+            int item = 5;
+
+            int[] result = (int[])array.Append(item);
+            Console.WriteLine(String.Join(",", result));
+        }
+    }
+}
